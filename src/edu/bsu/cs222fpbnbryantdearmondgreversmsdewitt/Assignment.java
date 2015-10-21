27bbe8a8ebb2b39d006fb8e1afc5e64aae4703e9
Matheu <msdewitt@bsu.edu>
2015-10-21 19:28:32 -0400

    Add dueDate and a unit test

--- src/edu/bsu/cs222fpbnbryantdearmondgreversmsdewitt/Assignment.java
+++ src/edu/bsu/cs222fpbnbryantdearmondgreversmsdewitt/Assignment.java
@@ -6,6 +6,7 @@ public class Assignment {
 	
 	public static class Builder {
 		private String name;
+		private LocalDate dueDate;
 		
 		public static Builder withName(String name) {
 			Builder builder = new Builder();
@@ -13,18 +14,30 @@ public class Assignment {
 			return builder;
 		}
 		
+		public Builder andDueDate(LocalDate dueDate) {
+			this.dueDate = dueDate;
+			return this;
+		}
+		
 		public Assignment build() {
 			return new Assignment(this);
 		}
+		
 	}
 	
 	private String name;
+	private LocalDate dueDate;
 	
 	private Assignment(Builder assignmentBuilder) {
 		name = assignmentBuilder.name;
+		dueDate = assignmentBuilder.dueDate;
 	}
 	
 	public String getName() {
 		return name;
+	}
+
+	public LocalDate getDueDate() {
+		return dueDate;
 	}
 }
--- test/edu/bsu/cs222fpbnbryantdearmondgreversmsdewitt/AssignmentTest.java
+++ test/edu/bsu/cs222fpbnbryantdearmondgreversmsdewitt/AssignmentTest.java
@@ -1,15 +1,26 @@
 package edu.bsu.cs222fpbnbryantdearmondgreversmsdewitt;
 
+import java.time.LocalDate;
+
 import org.junit.*;
 import edu.bsu.cs222fpbnbryantdearmondgreversmsdewitt.*;
 
 public class AssignmentTest {
-	private Assignment finalProject = Assignment.Builder.withName("Final Project").build();
+	private Assignment finalProject = Assignment.Builder//
+			.withName("Final Project")//
+			.andDueDate(LocalDate.of(2015, 12, 31))//
+			.build();
 
 	@Test
 	public void AssignmentNameIsFinalProject() {
 		String name = finalProject.getName();
 		Assert.assertEquals(name, "Final Project");
+	}
+
+	@Test
+	public void AssignmentDueDateIs31Dec2015() {
+		LocalDate testDate = LocalDate.of(2015, 12, 31);
+		Assert.assertEquals(testDate, finalProject.getDueDate());
 	}
 
 }
