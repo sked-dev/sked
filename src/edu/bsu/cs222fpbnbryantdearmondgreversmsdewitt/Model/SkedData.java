29e4634932c5d062a3cc207cbe8b50788c0172c8
Austin "Paco" DeArmond <hello@paco.audio>
2015-10-24 15:51:14 -0400

    Refactor SkedData to hold data independent from files

--- src/edu/bsu/cs222fpbnbryantdearmondgreversmsdewitt/Model/SkedData.java
+++ src/edu/bsu/cs222fpbnbryantdearmondgreversmsdewitt/Model/SkedData.java
@@ -1,24 +1,65 @@
 package edu.bsu.cs222fpbnbryantdearmondgreversmsdewitt.Model;
 
-public class SkedData {
+import java.util.UUID;
 
-		private final AssignmentList assignments;
+public class SkedData {
+	
+	private final AssignmentList assignments;
+	private final UUID uuid;
+	private final UserName name;
+	
+	public static class Builder {
+		private AssignmentList assignments = new AssignmentList();
+		private UserName name = UserName.unidentifiedUser();
 		
-		private SkedData(AssignmentList a) {
-			assignments = a;
+		public static Builder getBuilder() {
+			return new Builder();
 		}
 		
-		public static SkedData loadSavedUser() {
-			AssignmentList assignments = new AssignmentList();
-			return new SkedData(assignments);
+		public Builder withAssignmentList(AssignmentList assignments) {
+			this.assignments = assignments;
+			return this;
 		}
 		
-		public static boolean savedUserExists() {
-			return false;
+		public Builder withUserName(UserName name) {
+			this.name = name;
+			return this;
 		}
 		
-		public void saveUser() {
-			return;
+		public SkedData build() {
+			return new SkedData(this);
 		}
+	}
+	
+	private SkedData(Builder b) {
+		assignments = b.assignments;
+		name = b.name;
 		
+		uuid = UUID.randomUUID();
+	}
+
+	public static SkedData initialize() {
+		return new SkedData();
+	}
+	
+	private SkedData() {
+		assignments = new AssignmentList();
+		uuid = UUID.randomUUID();
+		name = UserName.unidentifiedUser();
+	}
+	
+	public AssignmentList getAssignments() {
+		return assignments;
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public UserName getName() {
+		return name;
+	}
+	
+	
+
 }
