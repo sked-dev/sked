d8d7471005865ea8d92aa7dd33ba8f1943d9a045
Austin "Paco" DeArmond <hello@paco.audio>
2015-10-25 01:26:55 -0400

    Fix #1, Assignment cannot be built with invalid configuration
    The builder defaults the start date to the current date.

--- src/edu/bsu/cs222fpbnbryantdearmondgreversmsdewitt/Model/Assignment.java
+++ src/edu/bsu/cs222fpbnbryantdearmondgreversmsdewitt/Model/Assignment.java
@@ -3,44 +3,61 @@ package edu.bsu.cs222fpbnbryantdearmondgreversmsdewitt.Model;
 import java.time.LocalDate;
 
 public class Assignment {
-	
+
 	public static class Builder {
-		private String name;
-		private LocalDate dueDate;
-		private LocalDate startDate;
-		
+		private String name = null;
+		private LocalDate dueDate = null;
+		private LocalDate startDate = LocalDate.now();
+
 		public static Builder withName(String name) {
 			Builder builder = new Builder();
 			builder.name = name;
 			return builder;
 		}
-		
+
 		public Builder andDueDate(LocalDate dueDate) {
 			this.dueDate = dueDate;
 			return this;
 		}
-		
-		public Builder andStartDate(LocalDate startDate){
-			this.startDate =startDate;
+
+		public Builder andStartDate(LocalDate startDate) {
+			this.startDate = startDate;
 			return this;
 		}
-		
+
 		public Assignment build() {
+			if (!ready()) {
+				throw new IllegalStateException("A required field has been left empty.");
+			}
 			return new Assignment(this);
 		}
-		
+
+		public boolean ready() {
+			return requiredFieldsAreSet();
+		}
+
+		private boolean requiredFieldsAreSet() {
+			if (name == null)
+				return false;
+			if (startDate == null)
+				return false;
+			if (dueDate == null)
+				return false;
+			return true;
+		}
+
 	}
-	
+
 	private String name;
 	private LocalDate dueDate;
 	private LocalDate startDate;
-	
+
 	private Assignment(Builder assignmentBuilder) {
 		name = assignmentBuilder.name;
 		dueDate = assignmentBuilder.dueDate;
 		startDate = assignmentBuilder.startDate;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
@@ -48,8 +65,8 @@ public class Assignment {
 	public LocalDate getDueDate() {
 		return dueDate;
 	}
-	
-	public LocalDate getStartDate(){
+
+	public LocalDate getStartDate() {
 		return startDate;
 	}
 }
--- test/edu/bsu/cs222fpbnbryantdearmondgreversmsdewitt/Model/AssignmentTest.java
+++ test/edu/bsu/cs222fpbnbryantdearmondgreversmsdewitt/Model/AssignmentTest.java
@@ -24,10 +24,27 @@ public class AssignmentTest {
 		LocalDate testDate = LocalDate.of(2015, 12, 31);
 		Assert.assertEquals(testDate, finalProject.getDueDate());
 	}
+
 	@Test
-	public void AssignmentStartDateIs29Feb2160(){
+	public void AssignmentStartDateIs29Feb2160() {
 		LocalDate testDate = LocalDate.of(2014, 3, 29);
 		Assert.assertEquals(testDate, finalProject.getStartDate());
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void builderWithUnsetNameThrowsIllegalStateException() {
+		Assignment.Builder b = new Assignment.Builder();
+		b.andDueDate(LocalDate.of(2015, 12, 31))//
+		.andStartDate(LocalDate.of(2015, 3, 29))//
+		.build();
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void builderWithUnsetDueDateThrowsIllegalStateException() {
+		Assignment.Builder//
+		.withName("Final Project")//
+		.andStartDate(LocalDate.of(2015, 3, 29))
+		.build();
 	}
 
 }
