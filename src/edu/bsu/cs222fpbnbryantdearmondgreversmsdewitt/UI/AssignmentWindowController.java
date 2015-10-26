c9bebea6e27c2a2e38f7a61be36417fd6835ba69
msdewitt <msdewitt@bsu.edu>
2015-10-26 17:59:16 -0400

    Add a assets folder with images and view controller

---
+++ src/edu/bsu/cs222fpbnbryantdearmondgreversmsdewitt/UI/AssignmentWindowController.java
@@ -0,0 +1,27 @@
+package edu.bsu.cs222fpbnbryantdearmondgreversmsdewitt.UI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class AssignmentWindowController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void loadAssignmentPaine(ActionEvent event) {
+
+    	
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
