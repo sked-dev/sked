9fa2ef656e00ea78ba6a4471b874c2ceeb12bf3f
Austin "Paco" DeArmond <hello@paco.audio>
2015-10-26 17:09:11 -0400

    Add sample FXML file

--- src/edu/bsu/cs222fpbnbryantdearmondgreversmsdewitt/UI/SkedApplication.java
+++ src/edu/bsu/cs222fpbnbryantdearmondgreversmsdewitt/UI/SkedApplication.java
@@ -1,12 +1,17 @@
 package edu.bsu.cs222fpbnbryantdearmondgreversmsdewitt.UI;
 
+import java.net.URL;
+
 import edu.bsu.cs222fpbnbryantdearmondgreversmsdewitt.Model.*;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class SkedApplication extends Application {
 	
-	private final SkedData data = SkedData.loadSavedUser();
+	private final SkedDataFile file = new SkedDataFile();
 
 	public static void main(String[] args) {
 		launch(args);
@@ -17,8 +22,17 @@ public class SkedApplication extends Application {
 		configure(primaryStage);
 	}
 
-	private void configure(Stage stage) {
+	private void configure(Stage stage) throws Exception {
 		stage.setTitle("SKED");
+		Scene scene = getMainScene();
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+
+	private Scene getMainScene() throws Exception {
+		Pane pane = (Pane) FXMLLoader.load(getClass().getResource("fxml_application_frame.fxml"));
+		return new Scene(pane);
 	}
 
 }
