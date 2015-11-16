8f5068f91f6a3e1a2134031cae5aca4f5bbf9eca
Austin "Paco" DeArmond <hello@paco.audio>
2015-11-16 15:46:54 -0500

    Add prioritization to AssignmentClass

--- src/edu/bsu/sked/model/AssignmentClass.java
+++ src/edu/bsu/sked/model/AssignmentClass.java
@@ -2,4 +2,29 @@ package edu.bsu.sked.model;
 
 public class AssignmentClass {
 	
+	private String name;
+	private boolean prioritized = false;
+	
+	public AssignmentClass(String name) {
+		this.name = validateName(name);
+	}
+
+	private String validateName(String name) {
+		if (name.isEmpty())
+			throw new IllegalArgumentException("Class name cannot be blank.");
+		return name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setPrioritized(boolean prioritized) {
+		this.prioritized = prioritized;
+	}
+	
+	public boolean isPrioritized() {
+		return prioritized;
+	}
+	
 }
--- test/edu/bsu/sked/model/AssignmentClassTest.java
+++ test/edu/bsu/sked/model/AssignmentClassTest.java
@@ -3,5 +3,30 @@ package edu.bsu.sked.model;
 import org.junit.*;
 
 public class AssignmentClassTest {
-	private AssignmentClass assignmentClass;
+	
+	@Test
+	public void testClassIsClas101() {
+		AssignmentClass assignmentClass = new AssignmentClass("CLAS 101");
+		Assert.assertEquals(assignmentClass.getName(), "CLAS 101");
+	}
+	
+	@Test
+	public void testClassIsEd200() {
+		AssignmentClass assignmentClass = new AssignmentClass("ED 200");
+		Assert.assertEquals(assignmentClass.getName(), "ED 200");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testEmptyClassNameThrowsIllegalArgumentException() {
+		new AssignmentClass("");
+	}
+	
+	@Test
+	public void testHardClassIsPrioritized() {
+		AssignmentClass hardClass = new AssignmentClass("HARD 999");
+		Assert.assertFalse(hardClass.isPrioritized());
+		hardClass.setPrioritized(true);
+		Assert.assertTrue(hardClass.isPrioritized());
+	}
+	
 }
