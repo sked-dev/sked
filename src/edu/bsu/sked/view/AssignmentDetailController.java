18b2b8fe9f9430822c79c23b517dc914fd179a00
Austin "Paco" DeArmond <hello@paco.audio>
2015-11-18 02:27:18 -0500

    Refactor

--- src/edu/bsu/sked/view/AssignmentDetailController.java
+++ src/edu/bsu/sked/view/AssignmentDetailController.java
@@ -4,8 +4,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import edu.bsu.sked.model.*;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
